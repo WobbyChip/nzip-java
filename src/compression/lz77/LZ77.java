45aa66509d318e55a5479a8a0992631fdf5bb2d2
WobbyChip <66483092+WobbyChip@users.noreply.github.com>
2024-03-13 16:28:50 +0200

    refactor code

--- src/compression/lz77/LZ77.java
+++ src/compression/lz77/LZ77.java
@@ -6,14 +6,18 @@ import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class LZ77 {
-    public static int MIN_DATA_LENGTH = 3; //This is because encoded reference\token uses 3 bytes
+    //This is because encoded reference uses 3 bytes + 1 bit while raw data uses 3 bytes and 3 bits
+    //Yes, we can set it to 3, because 25 < 27, but then we sacrifice length, so in average file it will get worse
+    public static int MIN_DATA_LENGTH = 4;
 
-    //256 + 3 => 255 + 4, which is 1 byte used in encoding, remember [0, 1, 2, 3] are never used in length
+    //255 + 4 => which is 1 byte used in encoding, remember [0, 1, 2, 3] are never used in length
     //So in the end we will have range [4; 259] - 4 => [0, 255]
-    public static int LOOK_AHEAD_BUFFER_SIZE = (1 << 8) + MIN_DATA_LENGTH;
+    public static int LOOK_AHEAD_BUFFER_SIZE = (1 << 8) - 1 + MIN_DATA_LENGTH; //256 - 1 + 4 = 259
 
     //Because of cycling data new possible range is [1, 65536] in case of 111111111 -> 1<8, 1> so we add +1
-    public static int SEARCH_BUFFER_SIZE = (1 << 16) + 1; //[0; 65535] which is 2 bytes used in encoding
+    public static int MIN_DATA_DISTANCE = 1;
+
+    public static int SEARCH_BUFFER_SIZE = (1 << 16) + MIN_DATA_DISTANCE; //[0; 65535] which is 2 bytes used in encoding
 
     public byte[] compress(byte[] data) {
         return compress(data, null);
@@ -25,16 +29,16 @@ public class LZ77 {
         BitCarry bitCarry = new BitCarry(); //Used to easily add data with ref bit
         int position = 0;
 
-        while (position < data.length - MIN_DATA_LENGTH - 1) {
+        while (position < data.length - MIN_DATA_LENGTH) {
             int[] reference = suffixArray.nextLongestMatch(position);
             int length = reference[0]; //Length of repeating data
 
-            if (length > MIN_DATA_LENGTH) {
+            if (length >= MIN_DATA_LENGTH) {
                 //If length more than 3 then encode distance and length and push them to bit carry
                 //-1 because (1 << 8): 256, 256 is out of range for byte [0; 255], and -MIN_DATA_LENGTH, because if (length > MIN_DATA_LENGTH)
-                int distance = position - reference[1] - 1; //Offset, aka, how much to go back
+                int distance = position - reference[1] - MIN_DATA_DISTANCE; //Offset, aka, how much to go back
                 //System.out.println("{ " + length + " " + (position - reference[1]) + " " + distance + " }");
-                bitCarry.pushBytes(true, ((length - MIN_DATA_LENGTH - 1) & 0xff), ((distance >> 8) & 0xff), (distance & 0xff));
+                bitCarry.pushBytes(true, ((length - MIN_DATA_LENGTH) & 0xff), ((distance >> 8) & 0xff), (distance & 0xff));
                 position += length;
             } else {
                 //If length less than tree, then push byte as normal
@@ -71,8 +75,8 @@ public class LZ77 {
                 return;
             }
 
-            int length = (e.data()[0] & 0xff) + MIN_DATA_LENGTH + 1;
-            int distance = ((e.data()[1] & 0xff) << 8 | (e.data()[2] & 0xff)) + 1;
+            int length = (e.data()[0] & 0xff) + MIN_DATA_LENGTH;
+            int distance = ((e.data()[1] & 0xff) << 8 | (e.data()[2] & 0xff)) + MIN_DATA_DISTANCE;
 
             for (int i = 0; i < length; i++) {
                 output.add(output.get(position.get() - distance + i));
